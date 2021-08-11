bed4eebb245ccf8690852a459413e8a9321667ff
Vladimir-Shutyak <vladimir.shutyak@mail.ru>
2021-08-11 08:00:49 +0300

    Lesson4 Task2. Adding Eslint

--- src/js/__tests__/health.test.js
+++ src/js/__tests__/health.test.js
@@ -1,10 +1,12 @@
 import userHealth from '../health';
+
 test.each([
-    ['Маг', 90, 'healthy'],
-    ['Маг', 40, 'wounded'],
-    ['Маг', 14, 'critical'],
+  ['Маг', 90, 'healthy'],
+  ['Маг', 40, 'wounded'],
+  ['Маг', 14, 'critical'],
 ])(
-('should show health'), (usrName, usrHealth, usrResult)=>{
-    const recieved = userHealth({name: usrName, health: usrHealth});
+  ('should show health'), (usrName, usrHealth, usrResult) => {
+    const recieved = userHealth({ name: usrName, health: usrHealth });
     expect(recieved).toBe(usrResult);
-});
+  },
+);
--- src/js/health.js
+++ src/js/health.js
@@ -1,5 +1,5 @@
-export default function userHealth(usr){
-    if (usr.health > 50) return ('healthy');
-    if ((usr.health <= 50) && (usr.health > 15)) return ('wounded');
-    return ('critical');
+export default function userHealth(usr) {
+  if (usr.health > 50) return ('healthy');
+  if ((usr.health <= 50) && (usr.health > 15)) return ('wounded');
+  return ('critical');
 }
